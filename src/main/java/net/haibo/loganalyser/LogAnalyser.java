53b2d6bc7054a594ffd10f113976decda52b3d73
Andy <andy.wang.haibo@gmail.com>
2014-09-22 06:32:48 +0800

    first workable version after test and bug fixes

--- src/main/java/net/haibo/loganalyser/LogAnalyser.java
+++ src/main/java/net/haibo/loganalyser/LogAnalyser.java
@@ -8,12 +8,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
@@ -43,10 +44,6 @@ public class LogAnalyser extends Configured implements Tool {
 			return 2;
 		}
 
-
-		System.out.println(String.format(
-				"Running LogAnalyser with input %s and output %s", args[0],
-				args[1]));
 		
 		Configuration conf = getConf();
 		Job job = Job.getInstance(conf, "log-analysis");
@@ -56,16 +53,19 @@ public class LogAnalyser extends Configured implements Tool {
 		NonSplitableTextInputFormat.setInputPaths(job, args[0]);
 		job.setInputFormatClass(NonSplitableTextInputFormat.class);
 		job.setMapperClass(RegexFilenameMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
 
 		job.setReducerClass(IdentityReducer.class);
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		job.setNumReduceTasks(1); // we do not need any reducer job
+		job.setNumReduceTasks(1); 
 
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
 
 	public static class RegexFilenameMapper extends
-			Mapper<Object, Text, Text, IntWritable> {
+			Mapper<LongWritable, Text, Text, IntWritable> {
 
 		private String fileName;
 		private Pattern badLinesPattern = Pattern
@@ -73,26 +73,24 @@ public class LogAnalyser extends Configured implements Tool {
 
 		@Override
 		protected void setup(
-				Mapper<Object, Text, Text, IntWritable>.Context context)
+				Mapper<LongWritable, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
-
 			super.setup(context);
-
-			fileName = context.getConfiguration().get(
-					MRJobConfig.MAP_INPUT_FILE);
+			
+			fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
 
 		}
 
 		@Override
-		protected void map(Object key, Text value,
-				Mapper<Object, Text, Text, IntWritable>.Context context)
+		protected void map(LongWritable key, Text value,
+				Mapper<LongWritable, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
-			super.map(key, value, context);
 
 			Matcher matcher = badLinesPattern.matcher(value.toString());
 			if (matcher.find()) {
 				context.write(new Text(fileName),
 						new IntWritable(Integer.parseInt(matcher.group(1))));
+				
 			}
 		}
 
